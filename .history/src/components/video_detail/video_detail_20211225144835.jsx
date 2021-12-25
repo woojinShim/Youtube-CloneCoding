26750a7760ee6734881a5a449e1103097aacbf52
Woojin <cmshim22@gmail.com>
2021-12-25 16:15:11 +0900

    youtube

--- .history/src/components/video_detail/video_detail_20211225144835.jsx
+++ .history/src/components/video_detail/video_detail_20211225144835.jsx
@@ -6,7 +6,6 @@ const VideoDetail = ({ video, video: { snippet } }) => (
     <iframe
       className={styles.video}
       type="text/html"
-      title="youtube video player"
       width="100%"
       height="500px"
       src={`https://www.youtube.com/embed/${video.id}`}
